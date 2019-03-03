64e448b4dddded0b255b4c52224ed2f61695d1a5
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-03-03 23:07:15 +0900

    都道府県一覧と市区町村一覧をテーブルから表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
- def index
- end
+  def index
+    @prefectures = Prefecture.order(list_order: :desc)
+  end
+  
+  def cities
+    prefecture_id = params[:prefecture_id]
+    @cities = City.where(prefecture_id: prefecture_id).order(list_order: :desc)
+  end
     
- def hospitals
+  def hospitals
     @id = params[:id]
- end
+  end
 end
